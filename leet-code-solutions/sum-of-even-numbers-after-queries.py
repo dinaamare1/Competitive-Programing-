fedee71591f9743e923859cbdca81616731389de
Dina Amare <dinaassefa04@gmail.com>
2023-12-12 13:51:39 +0300

    Added solution for Sum of Even Numbers After Queries

--- leet-code-solutions/sum-of-even-numbers-after-queries.py
+++ leet-code-solutions/sum-of-even-numbers-after-queries.py
@@ -2,31 +2,50 @@ class Solution:
     def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
         even = sum(x for x in nums if x % 2 == 0)
         result = []
+        for val, index in queries:
+            oldValue, nums[index] = nums[index], nums[index] + val
 
-        for val,index in queries:
-            if nums[index] % 2:
-                if (nums[index] + val) % 2 == 0:
-                    even += nums[index]+val
-                    nums[index] += val
-                    result.append(even)
-                else:
-                    nums[index] += val
-                    result.append(even)
-            else:
-                if (nums[index] + val) % 2 == 0:
-                    nums[index] += val
-                    even += val
-                    result.append(even)
-                else:
-                    even -= nums[index]
-                    nums[index] += val
-                    result.append(even)
-        print(nums)
-        return result 
+            if oldValue % 2 == 0:
+                even -= oldValue
+
+            if nums[index] % 2 == 0:
+                even += nums[index]
+
+            result.append(even)
+
+        return result
+
+
+
+
+        # the code is too long
+
+        # even = sum(x for x in nums if x % 2 == 0)
+        # result = []
+
+        # for val,index in queries:
+        #     if nums[index] % 2:
+        #         if (nums[index] + val) % 2 == 0:
+        #             even += nums[index]+val
+        #             nums[index] += val
+        #             result.append(even)
+        #         else:
+        #             nums[index] += val
+        #             result.append(even)
+        #     else:
+        #         if (nums[index] + val) % 2 == 0:
+        #             nums[index] += val
+        #             even += val
+        #             result.append(even)
+        #         else:
+        #             even -= nums[index]
+        #             nums[index] += val
+        #             result.append(even)
+        # return result 
 
         
 
-        
+        # Time limit excedded
         # sums = 0
         # result = []
         # for val,index in queries:
