b7adff638b5c778fe713ef84c50340b799d04c2f
Dina Amare <dinaassefa04@gmail.com>
2024-01-04 16:00:40 +0300

    Added solution for Boats to Save People

--- leet-code-solutions/boats-to-save-people.py
+++ leet-code-solutions/boats-to-save-people.py
@@ -10,3 +10,16 @@ class Solution:
             boat += 1
             r -= 1
         return boat
+        # people.sort()
+        # l = 0
+        # r = len(people)-1
+        # boat = 0
+        # while l<=r:
+        #     if people[l] + people[r] <= limit:
+        #         boat += 1
+        #         l+=1
+        #         r-=1
+        #     else:
+        #         boat += 1
+        #         r -= 1
+        # return boat
