3ebb7aeb4575fa3486e9d35b6041f3b519fdbb8f
Dina Amare <dinaassefa04@gmail.com>
2024-01-17 19:57:41 +0300

    Added solution for Product of Array Except Self

---
+++ leet-code-solutions/product-of-array-except-self.py
@@ -0,0 +1,19 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        mul = 1
+        count = nums.count(0)
+        ans = []
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                mul *= nums[i]
+        for i in range(len(nums)):
+            if count == 1 and nums[i] != 0:
+                ans.append(0)
+            elif count > 1:
+                ans.append(0)
+            elif nums[i] !=0:
+                ans.append(mul//nums[i])
+            else:
+                ans.append(mul)
+        return ans 
+        
