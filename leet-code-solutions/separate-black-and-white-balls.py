9a63b98fa9b353bc08e04a3c4139d54f9210be84
Dina Amare <dinaassefa04@gmail.com>
2024-02-07 21:29:18 +0300

    Added solution for Separate Black and White Balls

---
+++ leet-code-solutions/separate-black-and-white-balls.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        l = 0
+        r = len(s) -1 
+        nums = list(s)
+        count = 0
+        while l<r:
+            print(nums[l])
+            if nums[l] == "1" and nums[r] == "0":
+                nums[l],nums[r] = nums[r],nums[l]
+                count += r - l
+                l += 1
+                r -= 1
+            elif nums[r] == "1" and nums[l] == "1":
+                r -= 1
+            else:
+                l += 1
+        print(nums)
+        return count
