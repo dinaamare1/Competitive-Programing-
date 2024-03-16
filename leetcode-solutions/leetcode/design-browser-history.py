80340992c3ff29734b9c470441aa7ea7fe1636ca
Dina Amare <dinaassefa04@gmail.com>
2024-03-16 14:34:31 +0300

    Add solution for Design Browser History

--- leetcode-solutions/leetcode/design-browser-history.py
+++ leetcode-solutions/leetcode/design-browser-history.py
@@ -12,23 +12,32 @@ class BrowserHistory:
         self.history.next = curr
         self.history = curr
     def back(self, steps: int) -> str:
-        name = self.history
-        for i in range(steps):
-            if name.prev:
-                name = name.prev
-            else:
-                break
-        self.history = name
-        return name.val if name else None
+        # name = self.history
+        # for i in range(steps):
+        #     if name.prev:
+        #         name = name.prev
+        #     else:
+        #         break
+        # self.history = name
+        # return name.val if name else None
+        while self.history.prev and steps>0:
+            self.history = self.history.prev
+            steps -= 1
+        return self.history.val
+
     def forward(self, steps: int) -> str:
-        name = self.history
-        for i in range(steps):
-            if name.next:
-                name = name.next
-            else:
-                break
-        self.history = name
-        return name.val if name else None
+        # name = self.history
+        # for i in range(steps):
+        #     if name.next:
+        #         name = name.next
+        #     else:
+        #         break
+        # self.history = name
+        # return name.val if name else None
+        while self.history.next and steps>0:
+            self.history = self.history.next
+            steps -= 1
+        return self.history.val
 
         
 
